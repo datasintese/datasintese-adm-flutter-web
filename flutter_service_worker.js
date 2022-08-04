e8e699fc1ee4331f24ed6def5e48b6cf28c15d16
Sérgio Deivis <gtisergio@gmail.com>
2022-08-04 10:58:44 -0300

    up

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -6,7 +6,7 @@ const RESOURCES = {
   "assets/AssetManifest.json": "2efbb41d7877d10aac9d091f58ccd7b9",
 "assets/FontManifest.json": "dc3d03800ccca4601324923c0b1d6d57",
 "assets/fonts/MaterialIcons-Regular.otf": "95db9098c58fd6db106f1116bae85a0b",
-"assets/NOTICES": "d6d2269f3eba152af6ccecabddfba8e9",
+"assets/NOTICES": "13c94aee4f0b9be2cdb69c05aa3a1c01",
 "assets/packages/cupertino_icons/assets/CupertinoIcons.ttf": "6d342eb68f170c97609e9da345464e5e",
 "assets/shaders/ink_sparkle.frag": "ae6c1fd6f6ee6ee952cde379095a8f3f",
 "canvaskit/canvaskit.js": "2bc454a691c631b07a9307ac4ca47797",
@@ -19,9 +19,9 @@ const RESOURCES = {
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
 "icons/Icon-maskable-192.png": "c457ef57daa1d16f64b27b786ec2ea3c",
 "icons/Icon-maskable-512.png": "301a7604d45b3e739efc881eb04896ea",
-"index.html": "4b7391b70c169d01a6e381e41db5905b",
-"/": "4b7391b70c169d01a6e381e41db5905b",
-"main.dart.js": "3298228d8652accd446d16783e27c3a2",
+"index.html": "7f3892d398cba32d50f54027f3857934",
+"/": "7f3892d398cba32d50f54027f3857934",
+"main.dart.js": "9d6eaecdc7c5a01e333ec3dad5892905",
 "manifest.json": "5f6f741163e21af1352439f9150eb6e8",
 "version.json": "e4071fdd3c34314e29b650ef086f0ffe"
 };
